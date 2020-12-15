457eaced6b45f50d252bab3e74792280cbf5e5bb
Ian Homer <ian@purplepip.com>
2020-12-15 17:09:34 +0000

    Switch to typescript

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
@@ -10,18 +10,12 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1>
-          Title
-        </h1>
+        <h1>Title</h1>
 
-        <p>
-          Get started
-        </p>
+        <p>Get started</p>
 
-        <footer>
-           Footer
-        </footer>
+        <footer>Footer</footer>
       </main>
     </div>
-  )
+  );
 }
