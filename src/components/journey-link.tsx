5c40dc00a4dc884d6f2d3c635aae31acf828adf3
Ian Homer <ian@purplepip.com>
2020-12-29 17:46:04 +0000

    Create JourneyLink

---
+++ src/components/journey-link.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+interface JourneyLinkProps {
+  name: string;
+}
+
+export default function JourneyLink(props: JourneyLinkProps): JSX.Element {
+  return (
+    <Link href={props.name}>x</Link>
+  );
+}
+
--- src/components/journey.tsx
+++ src/components/journey.tsx
@@ -1,13 +1,13 @@
-import Link from "next/link";
+import JourneyLink from "./journey-link";
 
 export default function Journey(): JSX.Element {
   return (
     <>
-      <Link href="/">x</Link>
-      <Link href="/north-star">x</Link>
-      <Link href="/goals">x</Link>
-      <Link href="/reality-to-obvious">x</Link>
-      <Link href="/maps">x</Link>
+      <JourneyLink name="/"/>
+      <JourneyLink name="/north-star"/>
+      <JourneyLink name="/goals"/>
+      <JourneyLink name="/reality-to-obvious"/>
+      <JourneyLink name="/maps"/>
     </>
   );
 }
