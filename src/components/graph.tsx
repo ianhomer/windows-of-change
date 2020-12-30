f8f681405bc21558fc8daf0889c5607f14355fad
Ian Homer <ian@purplepip.com>
2020-12-30 12:02:35 +0000

    D3 force type experiment

---
+++ src/components/graph.tsx
@@ -0,0 +1,92 @@
+import { useEffect, useRef } from "react";
+import * as d3 from "d3";
+import * as d3Force from "d3-force";
+
+interface Graph {
+  nodes: d3Force.SimulationNodeDatum[];
+  links: d3Force.SimulationLinkDatum<d3Force.SimulationNodeDatum>[];
+}
+
+export default function Page() {
+  const ref = useRef(null);
+  const width = 400;
+  const height = 400;
+
+  const graph: Graph = {
+    nodes: [
+      { index: 0, x: 100, y: 100, vx: 0, vy: 0 },
+      { index: 1, x: 100, y: 200, vx: 0, vy: 0 },
+      { index: 2, x: 100, y: 200, vx: 0, vy: 0 },
+    ],
+    links: [
+      { source: 0, target: 1 },
+      { source: 1, target: 2 },
+    ],
+  };
+
+  useEffect(() => {
+    const svg = d3
+      .select(ref.current)
+      .append("svg")
+      .attr("width", width)
+      .attr("height", height);
+
+    const link = svg
+      .selectAll(".link")
+      .data(graph.links)
+      .join("line")
+      .classed("link", true);
+
+    const node = svg
+      .selectAll(".node")
+      .data(graph.nodes)
+      .join("circle")
+      .attr("r", 10)
+      .classed("node", true)
+      .classed("fixed", (d) => d.fx !== undefined);
+
+    function tick() {
+      link
+        .attr("x1", (d) => d.source.x)
+        .attr("y1", (d) => d.source.y)
+        .attr("x2", (d) => d.target.x)
+        .attr("y2", (d) => d.target.y);
+      node.attr("cx", (d) => d.x).attr("cy", (d) => d.y);
+    }
+
+    function click(event, d) {
+      delete d.fx;
+      delete d.fy;
+      d3.select(this).classed("fixed", false);
+      simulation.alpha(1).restart();
+    }
+
+    function clamp(x, lo, hi) {
+      return x < lo ? lo : x > hi ? hi : x;
+    }
+
+    const simulation = d3
+      .forceSimulation()
+      .nodes(graph.nodes)
+      .force("charge", d3.forceManyBody())
+      .force("center", d3.forceCenter(width / 2, height / 2))
+      .force("link", d3.forceLink(graph.links))
+      .on("tick", tick);
+
+    function dragstart() {
+      d3.select(this).classed("fixed", true);
+    }
+
+    function dragged(event, d) {
+      d.fx = clamp(event.x, 0, width);
+      d.fy = clamp(event.y, 0, height);
+      simulation.alpha(1).restart();
+    }
+
+    const drag = d3.drag().on("start", dragstart).on("drag", dragged);
+
+    node.call(drag).on("click", click);
+  }, []);
+
+  return <div ref={ref} />;
+}
