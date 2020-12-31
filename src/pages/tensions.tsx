5543e71609fbbc4a602d117b0c531f5628cf3bc5
Ian Homer <ian.homer@gmail.com>
2020-12-31 11:52:19 +0000

    Add things to do

--- src/pages/tensions.tsx
+++ src/pages/tensions.tsx
@@ -1,9 +1,40 @@
 import Layout from "../components/layout";
+import GraphDiagram from "../components/graph-diagram";
+import { Graph, NodeType } from "../types/graph";
+import { merge } from "lodash";
 
 const content = `
 - content
 `;
 
 export default function Page() {
-  return <Layout content={content} />;
+  // 3 Amigos
+  const graph: Graph = {
+    nodes: [
+      { index: 0, fx: 100, fy: 100, type: NodeType.Do, size: 20 },
+      { index: 1, type: NodeType.Do, size: 20 },
+      { index: 2, type: NodeType.Do, size: 20 },
+    ],
+    links: [],
+  };
+
+  // 3 Amigos with desired changes
+  const graphs = [graph];
+  graphs.push(
+    merge(graphs[0], {
+      nodes: [
+        {},
+        {},
+        {},
+        { index: 3, type: NodeType.Change, size: 10 },
+        { index: 4, type: NodeType.Change, size: 10 },
+      ],
+    })
+  );
+
+  return (
+    <Layout content={content}>
+      <GraphDiagram graph={graphs[1]} />
+    </Layout>
+  );
 }
