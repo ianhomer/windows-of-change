04378763c7e844967329da1e4518f72929696e5f
Ian Homer <ian@purplepip.com>
2020-12-31 18:08:48 +0000

    Changing text

--- src/pages/try/d3-changing-text.tsx
+++ src/pages/try/d3-changing-text.tsx
@@ -1,10 +1,19 @@
 import Layout from "../../components/layout";
 import ChangingText from "../../components/changing-text";
+import { useState } from "react";
 
 export default function Page() {
+  const [text, setText] = useState("test");
+
   return (
     <Layout>
-      <ChangingText>text</ChangingText>
+      <ChangingText>{text}</ChangingText>
+      <input
+        type="input"
+        onChange={(event) => {
+          setText(event.target.value);
+        }}
+      />
     </Layout>
   );
 }
