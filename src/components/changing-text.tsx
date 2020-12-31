6e2a76e664da114082e75580c992ffa0edb07252
Ian Homer <ian@purplepip.com>
2020-12-31 18:24:02 +0000

    Changing text minus transition

--- src/components/changing-text.tsx
+++ src/components/changing-text.tsx
@@ -11,32 +11,28 @@ export default function ChangingText(props: ChaningTextProps): JSX.Element {
   useEffect(() => {
     const svg = d3.select(ref.current);
 
-    const t = svg.transition().duration(750);
+    const text = svg.selectAll<SVGTextElement, string>("text");
 
-    svg
-      .selectAll("text")
+    //const transition = text.transition().duration(750);
+
+    text
       .data(props.children, (d) => d)
       .join(
         (enter) =>
           enter
             .append("text")
             .attr("x", (d, i) => 30 + i * 10)
-            .attr("y", 0)
+            .attr("y", 60)
             .attr("stroke", "white")
             .attr("fill", "white")
-            .text((d) => d)
-            .call((enter) => enter.transition(t).attr("y", 60)),
-        (update) =>
-          update
-            .attr("fill", "white")
-            .attr("y", 60)
-            .call((update) =>
-              update.transition(t).attr("x", (d, i) => 30 + i * 10)
-            ),
-        (exit) =>
-          exit
-            .attr("fill", "brown")
-            .call((exit) => exit.transition(t).attr("y", 300).remove())
+            .text((d) => d),
+        //.call((enter) => enter.transition(transition).attr("y", 60)),
+        (update) => update.attr("fill", "white").attr("y", 60),
+        //.call((update) =>
+        //  update.transition(transition).attr("x", (d, i) => 30 + i * 10)
+        //)
+        (exit) => exit.attr("fill", "brown")
+        //.call((exit) => exit.transition(transition).attr("y", 300).remove())
       );
 
     svg.node();
