7bfe5120b9cbcc6be17fd0b6385171c753560b34
Ian Homer <ian@purplepip.com>
2021-01-01 14:46:23 +0000

    Abstract journey

--- src/components/journey.tsx
+++ src/components/journey.tsx
@@ -1,5 +1,6 @@
 import JourneyLink from "./journey-link";
 import styled from "styled-components";
+import windowsOfChange from "../journeys/windows-of-change";
 
 const Wrapper = styled.div`
   display: block;
@@ -10,34 +11,9 @@ const Wrapper = styled.div`
 export default function Journey(): JSX.Element {
   return (
     <Wrapper>
-      <JourneyLink name="" />
-      <JourneyLink name="start" />
-      <JourneyLink name="do" />
-      <JourneyLink name="happiness" />
-      <JourneyLink name="north-star" />
-      <JourneyLink name="goals" />
-      <JourneyLink name="reality-to-obvious" />
-      <JourneyLink name="maps" />
-      <JourneyLink name="cloudy-future" />
-      <JourneyLink name="pivot" />
-      <JourneyLink name="silos" />
-      <JourneyLink name="guard-rails" />
-      <JourneyLink name="how-to-contribute" />
-      <JourneyLink name="things-change-do" />
-      <JourneyLink name="trunk" />
-      <JourneyLink name="tensions" />
-      <JourneyLink name="me-to-org" />
-      <JourneyLink name="todo" />
-      <JourneyLink name="delivery-lead" />
-      <JourneyLink name="product-lead" />
-      <JourneyLink name="technical-lead" />
-      <JourneyLink name="worth-knowing" />
-      <JourneyLink name="turn-up-the-good" />
-      <JourneyLink name="delete-something" />
-      <JourneyLink name="wip" />
-      <JourneyLink name="wrap" />
-      <JourneyLink name="try/d3-force" />
-      <JourneyLink name="try/board-1" />
+      {windowsOfChange.map((name) => (
+        <JourneyLink name={name} />
+      ))}
     </Wrapper>
   );
 }
--- src/pages/goals.tsx
+++ src/pages/goals.tsx
@@ -1,16 +1,13 @@
 import Layout from "../components/layout";
-import dynamic from "next/dynamic";
+import lesson from "../lessons/goals";
+//import dynamic from "next/dynamic";
 
-const DynamicComponent = dynamic(() =>
-  import("../lessons/goals").then((mod) => () => (
-    <Layout lesson={mod.default} />
-  ))
-);
+//const DynamicComponent = dynamic(() =>
+//import("../lessons/goals").then((mod) => () => (
+//<Layout lesson={mod.default} />
+//))
+//);
 
 export default function Page() {
-  return (
-    <>
-      <DynamicComponent />
-    </>
-  );
+  return <Layout lesson={lesson} />;
 }
