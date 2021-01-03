58ee6c845fe507ba2c44b45dc38a254e84c39822
Ian Homer <ian@purplepip.com>
2021-01-03 15:47:48 +0000

    Step through quotes

--- src/pages/happiness.tsx
+++ src/pages/happiness.tsx
@@ -1,14 +1,42 @@
 import Layout from "../components/layout";
 import Quote from "../components/quote";
 import lesson from "../lessons/happiness";
+import { useEffect, useState } from "react";
+import { transition } from "../utils/journeyish";
 
-export default function Page() {
-  return (
-    <Layout notes={lesson.notes}>
+const positions = [
+  {
+    quote: (
       <Quote who="Mahatma Gandhi">
         Happiness is when what you think, what you say, and what you do are in
         harmony.
       </Quote>
+    ),
+  },
+  {
+    quote: (
+      <Quote who="Joe Wicks">
+        Don't look too far into the future, just look at tomorrow. One day at a
+        time
+      </Quote>
+    ),
+  },
+];
+
+export default function Page() {
+  const [position, setPosition] = useState(0);
+  const [quote, setQuote] = useState(positions[0].quote);
+
+  useEffect(() => {
+    setQuote(positions[position].quote);
+  }, [position]);
+
+  return (
+    <Layout
+      notes={lesson.notes}
+      transition={transition(positions.length, setPosition)}
+    >
+      {quote}
     </Layout>
   );
 }
