6990fca2bc21cee8d7ea0f1a4f8451f81d1a8dcb
Ian Homer <ian@purplepip.com>
2021-01-03 15:16:06 +0000

    Reality stack

--- src/pages/reality-to-obvious.tsx
+++ src/pages/reality-to-obvious.tsx
@@ -1,19 +1,41 @@
 import Layout from "../components/layout";
 import Stack from "../components/stack";
 import lesson from "../lessons/reality-to-obvious";
+import { useEffect, useState } from "react";
+import { transition } from "../utils/journeyish";
+
+const positions = [
+  { properties: ["name"] },
+  { properties: ["name", "ego"] },
+  { properties: ["name", "time"] },
+  { properties: ["name", "ego", "time"] },
+];
 
 export default function Page() {
+  const [position, setPosition] = useState(0);
+  const [sky, setSky] = useState("reality");
+  const [properties, setProperties] = useState(["name"]);
+
+  useEffect(() => {
+    setProperties(positions[position].properties);
+    setSky(position == 0 ? "reality" : "unknowable");
+  }, [position]);
+
   return (
-    <Layout lesson={lesson}>
+    <Layout
+      lesson={lesson}
+      transition={transition(positions.length, setPosition)}
+    >
       <Stack
-        sky="Reality"
+        properties={properties}
+        sky={sky}
         layers={[
-          { name: "Experience" },
-          { name: "Attention" },
-          { name: "Theories" },
-          { name: "Judgments" },
-          { name: "Beliefs" },
-          { name: "The Obvious" },
+          { name: "Experience", ego: "observable", time: "future" },
+          { name: "Attention", ego: "desires", time: "plans" },
+          { name: "Theories", ego: "decisions" },
+          { name: "Judgments", ego: "bets" },
+          { name: "Beliefs", time: "next" },
+          { name: "The Obvious", ego: "knowable", time: "now" },
         ]}
       />
     </Layout>
