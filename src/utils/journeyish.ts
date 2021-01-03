dd2419917ad0632e02004356c688e37ba83b306a
Ian Homer <ian@purplepip.com>
2021-01-03 08:37:50 +0000

    Tests pass

--- src/utils/journeyish.ts
+++ src/utils/journeyish.ts
@@ -1,7 +1,23 @@
 export default function travel(
   steps: string[],
-  path: path,
+  current: string,
   direction: number
 ): string {
-  return "start";
+  if (direction == 0) {
+    return current;
+  }
+  const currentIndex = steps.findIndex((s) => s == current);
+  let nextIndex;
+  // Forward always goes forward, even if not found
+  if (direction > 0 && currentIndex < 0) {
+    nextIndex = steps.length - 1;
+  } else {
+    nextIndex = currentIndex + direction;
+    if (nextIndex < 0) {
+      nextIndex = 0;
+    } else if (nextIndex > steps.length - 1) {
+      nextIndex = steps.length - 1;
+    }
+  }
+  return steps[nextIndex];
 }
