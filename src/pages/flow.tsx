fe6174fce5753a59493b8e149a80a83aa0cff7f5
Ian Homer <ihomer@worth.systems>
2021-01-05 17:36:24 +0000

    Add positions to flow

--- src/pages/flow.tsx
+++ src/pages/flow.tsx
@@ -2,28 +2,56 @@ import Layout from "../components/layout";
 import lesson from "../lessons/flow";
 import GraphDiagram from "../components/graph-diagram";
 import { Graph, NodeType } from "../types/graph";
+import { useEffect, useState } from "react";
+import { transition } from "../utils/journeyish";
 
-export default function Page() {
-  const graph: Graph = {
+const positions = [
+  { dos: false, goals: false },
+  { dos: true, goals: false },
+  { dos: true, goals: true },
+];
+
+const createGraph = (position = { dos: false, goals: false }): Graph => {
+  return {
     nodes: [
       { id: "thing-1", fx: 500, fy: 200, type: NodeType.Thing, size: 40 },
       { id: "change-1", type: NodeType.Change, size: 20 },
       { id: "change-2", type: NodeType.Change, size: 20 },
-      { id: "do-1", type: NodeType.Do, size: 10 },
-      { id: "do-2", type: NodeType.Do, size: 10 },
-      { id: "do-3", type: NodeType.Do, size: 10 },
+      ...(position.dos
+        ? [
+            { id: "do-1", type: NodeType.Do, size: 10 },
+            { id: "do-2", type: NodeType.Do, size: 10 },
+            { id: "do-3", type: NodeType.Do, size: 10 },
+          ]
+        : []),
     ],
     links: [
       { target: "change-1", source: "thing-1", size: 4 },
       { target: "change-2", source: "thing-1" },
-      { target: "do-1", source: "change-1", size: 3 },
-      { target: "do-2", source: "change-1" },
-      { target: "do-3", source: "change-2" },
+      ...(position.dos
+        ? [
+            { target: "do-1", source: "change-1", size: 3 },
+            { target: "do-2", source: "change-1" },
+            { target: "do-3", source: "change-2" },
+          ]
+        : []),
     ],
   };
+};
+
+export default function Page() {
+  const [position, setPosition] = useState(0);
+  const [graph, setGraph] = useState(createGraph());
+
+  useEffect(() => {
+    setGraph(createGraph(positions[position]));
+  }, [position]);
 
   return (
-    <Layout lesson={lesson}>
+    <Layout
+      lesson={lesson}
+      transition={transition(positions.length, setPosition)}
+    >
       <GraphDiagram graph={graph} />
     </Layout>
   );
