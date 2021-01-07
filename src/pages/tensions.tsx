0a9e382dc8048fcb77b12dac6f2e40ea23d8ee8c
Ian Homer <ian@purplepip.com>
2021-01-07 22:46:21 +0000

    Label tensions

--- src/pages/tensions.tsx
+++ src/pages/tensions.tsx
@@ -10,17 +10,53 @@ export default function Page() {
   const [checked, setChecked] = useState(false);
   const [graph, setGraph] = useState<Graph>({
     nodes: [
-      { id: "pl", index: 0, x: 0, y: -100, type: NodeType.Do, size: 20 },
-      { id: "dl", index: 1, x: -100, y: 100, type: NodeType.Do, size: 20 },
-      { id: "tl", index: 2, x: 100, y: 100, type: NodeType.Do, size: 20 },
+      {
+        id: "pl",
+        index: 0,
+        x: 0,
+        y: -150,
+        type: NodeType.Do,
+        size: 20,
+        label: "Product Lead",
+      },
+      {
+        id: "dl",
+        index: 1,
+        x: -150,
+        y: 150,
+        type: NodeType.Do,
+        size: 20,
+        label: "Technical Lead",
+      },
+      {
+        id: "tl",
+        index: 2,
+        x: 150,
+        y: 150,
+        type: NodeType.Do,
+        size: 20,
+        label: "Delivery Lead",
+      },
     ],
     links: [],
   });
 
   const onChange = (checked: boolean) => {
     const newNodes = [
-      { id: "change1", index: 3, type: NodeType.Change, size: 10 },
-      { id: "change2", index: 4, type: NodeType.Change, size: 10 },
+      {
+        id: "change1",
+        index: 3,
+        type: NodeType.Change,
+        size: 10,
+        label: "change",
+      },
+      {
+        id: "change2",
+        index: 4,
+        type: NodeType.Change,
+        size: 10,
+        label: "change",
+      },
     ];
     setGraph(
       checked ? addNodes(graph, newNodes) : removeNodes(graph, newNodes)
