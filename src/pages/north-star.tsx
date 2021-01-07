1694a6d7c8cd3dec7d444b42964184566c2abb05
Ian Homer <ian@purplepip.com>
2021-01-07 22:33:12 +0000

    Correct image width

--- src/pages/north-star.tsx
+++ src/pages/north-star.tsx
@@ -4,12 +4,20 @@ import styled from "styled-components";
 
 const Image = styled.img`
   border: 1px solid #999;
+  @media (min-width: 800px) {
+    width: 800px;
+  }
+  @media (max-width: 800px) {
+    width: 90%;
+  }
+
+  width: 800px;
 `;
 
 export default function Page() {
   return (
     <Layout notes={lesson.notes}>
-      <Image width={800} src="https://bit.ly/3n6WbHX" />
+      <Image src="https://bit.ly/3n6WbHX" />
     </Layout>
   );
 }
