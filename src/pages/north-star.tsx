11254d98cbbc1fec5376af34f6456f09fa66851d
Ian Homer <ihomer@worth.systems>
2021-01-09 17:49:20 +0000

    North star as diagram

--- src/pages/north-star.tsx
+++ src/pages/north-star.tsx
@@ -2,22 +2,22 @@ import Layout from "../components/layout";
 import lesson from "../lessons/north-star";
 import styled from "styled-components";
 
-const Image = styled.img`
-  border: 1px solid #999;
-  @media (min-width: 800px) {
-    width: 800px;
-  }
-  @media (max-width: 800px) {
-    width: 90%;
-  }
-
+const Image = styled.div`
+  box-shadow: 0 0 8px 8px #0e253a inset;
+  background-image: url("https://bit.ly/3n6WbHX");
+  background-size: 800px;
+  background-repeat: no-repeat;
   width: 800px;
+  height: 2000px;
+  position: absolute;
+  top: 5em;
+  left: 20em;
 `;
 
 export default function Page() {
   return (
     <Layout notes={lesson.notes}>
-      <Image src="https://bit.ly/3n6WbHX" />
+      <Image />
     </Layout>
   );
 }
