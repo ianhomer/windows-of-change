84d7283082cfe6005eaed123654a72c880443664
Ian Homer <ihomer@worth.systems>
2021-01-09 13:08:00 +0000

    Tweak flow

--- src/pages/flow.tsx
+++ src/pages/flow.tsx
@@ -27,17 +27,26 @@ const createGraph = (position = { dos: false, goals: false }): Graph => {
       { id: "change-3", x: -200, y: 0, type: NodeType.Change, size: 20 },
       ...(position.dos
         ? [
-            { id: "do-1", x: 200, y: 0, type: NodeType.Do, size: 10 },
-            { id: "do-2", x: 200, y: 0, type: NodeType.Do, size: 10 },
+            {
+              id: "do-1",
+              x: 200,
+              y: 0,
+              type: NodeType.Do,
+              size: 10,
+              label: "do",
+            },
+            { id: "do-2", x: 200, y: 0, type: NodeType.Do, size: 20 },
             { id: "do-3", x: -100, y: 0, type: NodeType.Do, size: 10 },
+            { id: "do-5", x: -100, y: 0, type: NodeType.Do, size: 5 },
+            { id: "do-4", x: -100, y: 0, type: NodeType.Do, size: 5 },
           ]
         : []),
       ...(position.goals
         ? [
             {
               id: "goal-1",
-              fx: 200,
-              fy: 0,
+              x: 300,
+              y: -100,
               type: NodeType.Change,
               size: 40,
               label: "goal",
@@ -54,11 +63,13 @@ const createGraph = (position = { dos: false, goals: false }): Graph => {
             { target: "do-1", source: "change-1" },
             { target: "do-2", source: "change-1" },
             { target: "do-3", source: "change-2" },
+            { target: "do-4", source: "change-2" },
           ]
         : []),
       ...(position.goals
         ? [
-            { target: "change-1", source: "goal-1" },
+            { target: "change-2", source: "goal-1" },
+            { target: "do-5", source: "goal-1" },
             { target: "goal-1", source: "thing-1" },
           ]
         : []),
