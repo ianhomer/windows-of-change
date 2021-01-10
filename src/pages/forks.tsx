675491e1bf080d54a5feb44118f3af76eefe9911
Ian Homer <ian@purplepip.com>
2021-01-10 17:34:39 +0000

    Forks

--- src/pages/forks.tsx
+++ src/pages/forks.tsx
@@ -2,5 +2,10 @@ import Layout from "../components/layout";
 import lesson from "../lessons/forks";
 
 export default function Page() {
-  return <Layout lesson={lesson} />;
+  return (
+    <Layout
+      lesson={lesson}
+      background="https://live.staticflickr.com/2940/13908169649_f9c0114fbb_k.jpg"
+    />
+  );
 }
