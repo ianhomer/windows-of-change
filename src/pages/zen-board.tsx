51afbabddc19731e317a646cd226a48254dd945d
Ian Homer <ian@purplepip.com>
2021-01-10 20:45:13 +0000

    Zen board

--- src/pages/zen-board.tsx
+++ src/pages/zen-board.tsx
@@ -1,6 +1,21 @@
 import Layout from "../components/layout";
 import lesson from "../lessons/zen-board";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import Board from "../components/board";
 
 export default function Page() {
-  return <Layout lesson={lesson} />;
+  const contents = [
+    { left: 100, top: 100, children: "do" },
+    { left: 300, top: 100, children: "doing" },
+    { left: 500, top: 100, children: "done" },
+  ];
+
+  return (
+    <Layout notes={lesson.notes}>
+      <DndProvider backend={HTML5Backend}>
+        <Board contents={contents} />
+      </DndProvider>
+    </Layout>
+  );
 }
