b0feab6828eb76d7286d163553c3fc0e56f980d4
Ian Homer <ian@purplepip.com>
2021-01-11 07:17:16 +0000

    Add drag and drop component

---
+++ src/components/drag-and-drop.tsx
@@ -0,0 +1,33 @@
+// As per https://github.com/react-dnd/react-dnd/issues/186#issuecomment-561631584
+
+import { DndProvider, createDndContext } from "react-dnd";
+import HTML5Backend from "react-dnd-html5-backend";
+import React, { useRef } from "react";
+
+interface DragAndDropProps {
+  left?: number;
+  top?: number;
+  width?: number;
+  height?: number;
+  children: any;
+  opacity: number;
+}
+
+const RNDContext = createDndContext(HTML5Backend);
+
+function useDNDProviderElement(props: DragAndDropProps) {
+  const manager = useRef(RNDContext);
+
+  if (!props.children) return null;
+
+  return (
+    <DndProvider manager={manager.current.dragDropManager}>
+      {props.children}
+    </DndProvider>
+  );
+}
+
+export default function DragAndDrop(props: DragAndDropProps) {
+  const DNDElement = useDNDProviderElement(props);
+  return <React.Fragment>{DNDElement}</React.Fragment>;
+}
