087041a47b40ee4b0d8d5cc354414a452b0a15cd
Ian Homer <ian.homer@gmail.com>
2021-01-12 00:19:03 +0000

    Health checks

--- src/pages/principles.tsx
+++ src/pages/principles.tsx
@@ -11,7 +11,7 @@ export default function Page() {
           {},
           { name: "principles" },
           { name: "guardrails" },
-          { name: "healthchecks" },
+          { name: "health checks" },
           { name: "practices" },
         ]}
       />
