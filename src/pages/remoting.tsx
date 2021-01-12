bbd6b852fec13f38ca36609640e433f0c20c0520
Ian Homer <ian.homer@gmail.com>
2021-01-12 00:06:41 +0000

    Slides

--- src/pages/remoting.tsx
+++ src/pages/remoting.tsx
@@ -5,18 +5,18 @@ import Board from "../components/board";
 
 export default function Page() {
   const contents = [
-    { left: 300, top: 0, children: "who's where", opacity: 0.4 },
+    { left: 300, top: 0, children: "who's where? heads down?", opacity: 0.4 },
     { left: 500, top: 0, children: "break outs", opacity: 0.4 },
-    { left: 100, top: 200, children: "what's the buzz", opacity: 0.6 },
+    { left: 100, top: 200, children: "what's the buzz?", opacity: 0.6 },
     {
       left: 300,
       top: 200,
-      children: "who wants to ask you something",
+      children: "who wants to ask you something?",
       opacity: 0.6,
     },
     { left: 500, top: 200, children: "open calls" },
-    { left: 700, top: 200, children: "who's chilling", opacity: 0.8 },
-    { left: 100, top: 400, children: "who's starting", opacity: 0.6 },
+    { left: 700, top: 200, children: "who's chilling?", opacity: 0.8 },
+    { left: 100, top: 400, children: "who's starting?", opacity: 0.6 },
     { left: 900, top: 400, children: "explosions", opacity: 0.4 },
     { left: 500, top: 400, children: "7-bone or Thai?" },
   ];
